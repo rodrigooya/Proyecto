f489889b9fccb875b7f740fa29856102d3df311a
Rodrigooya <oyarzun.rodrigoant@gmail.com>
2021-09-03 19:14:28 -0400

    Update docente.rb

--- tarea/docente.rb
+++ tarea/docente.rb
@@ -6,11 +6,8 @@ class Docente
         @grado = grado
         @antiguedad = antiguedad
     end
-    def datos
-        puts "el docente de nombre #{@nombre} #{@apellido} rut #{@rut} es de grado #{@grado} tiene una antiguedad de #{@antiguedad}" 
-    end
 end
 
-juan = Docente.new("0001-9","Juan","Perez","Bachiller",6)
-juan.datos
+
+
 
