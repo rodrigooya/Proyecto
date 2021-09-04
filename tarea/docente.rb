0186fbea7eb4f390731394b53b2dff01ee9f9112
Rodrigooya <oyarzun.rodrigoant@gmail.com>
2021-09-03 21:11:51 -0400

    Update docente.rb

--- tarea/docente.rb
+++ tarea/docente.rb
@@ -1,10 +1,12 @@
 class Docente
-    def initialize (rut,nombre,apellido,grado,antiguedad)
+    def initialize (rut,nombre,apellido,grado,antiguedad,horas,tardanza)
         @rut = rut
         @nombre = nombre
         @apellido = apellido
         @grado = grado
         @antiguedad = antiguedad
+        @horas = horas
+        @tardanza = tardanza
     end
 end
 
